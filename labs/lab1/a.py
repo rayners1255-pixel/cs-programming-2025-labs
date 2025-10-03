0faaa125c29fda804b8982064f38eee0f51d47bf
rayners1255-pixel <rayners1255@gmail.com>
2025-10-03 13:11:33 +1000

    Create a.py

---
+++ labs/lab1/a.py
@@ -0,0 +1,69 @@
+# #1
+# a = 15
+# b = 15.56
+# c = "Я Рэйнер"
+# d = True
+# #2
+# Name = "Рэйнер"
+# old = 18
+# #3
+# a1 = 342
+# a2 = 56.2
+# a3 = "43"
+# rez = float(a1) + a2 + float(a3)
+# print(rez)
+# #4
+# a,b = 3,8
+# print((a+4*b)*(a-3*b) + a**2)
+# #5
+# stor1 = int(input("Vod: "))
+# stor2 = int(input("Vod: "))
+# print(f"Площадь: {stor1*stor2}; Периметр: {(stor1*2) + (stor2*2)}")
+# #6
+# print("*   *   *")
+# print(" * * * * ")
+# print("  *  *")
+# #7
+# a = 10
+# b = 12
+# print(f"a = {a}; b = {b}    "
+#       f"; a*b = {a*b}; a+b = {a+b}; a-b = {a-b}; "
+#       f"a/b = {a/b}; a//b = {a//b}; a%b = {a%b}"
+#       f"a**b = {a**b}; ")
+# print(f"a = {a}; b = {b} ;a==b: {a==b}; a>b: {a>b}; a<b: {a<b}; a!=b: {a!=b}; "
+#       f"a<=b: {a<=b}; a>=b: {a>=b}")
+
+# #8
+# NameMy = "Рэйнер"
+# OldMy = 18
+# print(F"Меня зовут {NameMy}, мне {OldMy} лет.")
+# #9
+# sochnieBulki = "Съешь ещё этих мягких французских булок, да выпей чаю"
+# b1 = "Съешь "
+# b2 = "ещё "
+# b3 = "этих "
+# b4 = "мягких "
+# b5 = "французских "
+# b6 = "булок, "
+# b7 = "да выпей чаю"
+#
+# print(b1+b2+b3+b4+b5+b6+b7)
+# #10
+# print("Нет! Да! "*4)
+# #11
+# inp1 = input()
+# p1,p2,p3 = inp1.split(",")
+# print(F"Результат вычесления: {(int(p1)+int(p3))//int(p2)}")
+#12
+slovo = "Гидроперфоратор"
+inp000 = input("Vod: ")
+print(inp000[:4])
+print(inp000[-2]+inp000[-1])
+print(inp000[3:8])
+print(inp000[::-1])
+
+
+
+
+
+
